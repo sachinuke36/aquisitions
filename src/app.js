34759083a33938f32b758903f2341343e3d0de1b
sachinuke36 <sachinuke36@gmail.com>
2026-01-02 13:45:49 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+ const app = express();
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the Aquisitions Service!');
+});
+
+export default app;
+
+
