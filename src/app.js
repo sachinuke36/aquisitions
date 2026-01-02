1d8f91f734e9566438e8b84f3c1c5294f4b79dd4
sachinuke36 <sachinuke36@gmail.com>
2026-01-02 14:01:33 +0530

    add: ESlint and prettier

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 import express from 'express';
 
-
- const app = express();
-
+const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the Aquisitions Service!');
 });
 
 export default app;
-
-
--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 import app from './app.js';
 
-
-
 const PORT = process.env.PORT || 3000;
-
 
 app.listen(PORT, () => {
   console.log(`Aquisitions Service is running on http://localhost:${PORT}`);
